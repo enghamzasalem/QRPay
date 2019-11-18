374bda6bb0dacf785eb5c76cc86e8766e2369403
Hamza <h.salem@portavita.ru>
2019-11-18 15:22:40 +0300

    build First functionalty

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,49 +1,37 @@
-import { Component } from '@angular/core';
-import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
-
+import {Component} from "@angular/core";
+import {BarcodeScanner, BarcodeScannerOptions} from "@ionic-native/barcode-scanner/ngx";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
   scannedData: any;
-  encodedData = '';
-  constructor(public barcodeCtrl: BarcodeScanner) { }
+  encodedData = "";
+  constructor(public barcodeCtrl: BarcodeScanner) {}
   goToBarcodeScan() {
     const options: BarcodeScannerOptions = {
       preferFrontCamera: false,
       showFlipCameraButton: true,
       showTorchButton: true,
       torchOn: false,
-      prompt: 'Place a barcode inside the scan area',
+      prompt: "Place a barcode inside the scan area",
       resultDisplayDuration: 500,
-      formats: 'QR_CODE,PDF_417 ',
-      orientation: 'landscape',
+      formats: "QR_CODE,PDF_417 ",
+      orientation: "landscape"
     };
 
-    this.barcodeCtrl.scan(options).then(barcodeData => {
-      console.log('Barcode data', barcodeData);
-      this.scannedData = barcodeData;
-
-    }).catch(err => {
-      console.log('Error', err);
-    });
+    this.barcodeCtrl
+      .scan(options)
+      .then(barcodeData => {
+        console.log("Barcode data", barcodeData);
+        this.scannedData = barcodeData;
+        //alert(this.scannedData);
+        // window.location.href = this.scannedData;
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
   }
-
-
-
-  goToCreateCode() {
-    this.barcodeCtrl.encode(this.barcodeCtrl.Encode.TEXT_TYPE, this.encodedData).then((encodedData) => {
-      console.log(encodedData);
-      this.encodedData = encodedData;
-    }, (err) => {
-      console.log('Error occured : ' + err);
-    });
-  }
-
 }
-
-
-
